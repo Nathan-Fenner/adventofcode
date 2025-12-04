268e43489d876c5204c7ffdce464e284b899aace
Nathan Fenner <nfenneremail@gmail.com>
2025-12-03 21:04:19 -0800

    part 2

--- day4.ts
+++ day4.ts
@@ -31,16 +31,29 @@ console.info(input);
 const g = parseGrid(input);
 
 let count = 0;
-for (const p of g.grid) {
-  if (p[1] === "@") {
-    let block = 0;
-    for (const n of p[0].neighborsDiag()) {
-      if (g.grid.get(n) === "@") {
-        block += 1;
-      }
+
+const recheck = new Set<Pos2>();
+for (const p of g.grid.keys()) {
+  recheck.add(p);
+}
+
+for (const p of recheck) {
+  recheck.delete(p);
+  if (g.grid.get(p) !== "@") {
+    continue;
+  }
+
+  let block = 0;
+  for (const n of p.neighborsDiag()) {
+    if (g.grid.get(n) === "@") {
+      block += 1;
     }
-    if (block < 4) {
-      count += 1;
+  }
+  if (block < 4) {
+    count += 1;
+    g.grid.set(p, "_");
+    for (const n of p.neighborsDiag()) {
+      recheck.add(n);
     }
   }
 }
